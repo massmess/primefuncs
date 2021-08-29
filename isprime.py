67099a9d01dc13c5bda5297fdb2449ea66b7af03
五叔 <fifthuncle@outlook.com>
2021-08-29 09:41:10 +0800

    Indent using tabs (4 spaces)

--- isprime.py
+++ isprime.py
@@ -4,25 +4,25 @@ print(isprime.__doc__)
 print("-- Type 'QUIT' to end the test.")
 
 while True:
-  s = input("-- Enter a number for the test: ")
+	s = input("-- Enter a number for the test: ")
 
-  if s.upper() == 'QUIT':
-    print("Done.")
-    break
+	if s.upper() == 'QUIT':
+		print("Done.")
+		break
 
-  try:
-    n = int(s)
-  except ValueError:
-    print("Wrong number (or not one)! Try again!")
-    continue
+	try:
+		n = int(s)
+	except ValueError:
+		print("Wrong number (or not one)! Try again!")
+		continue
 
-  if n <= 0:
-    print("Number must be non-zero, non-negative!")
-  else:
-    print("Checking...")
-    y = isprime(n)
-    print(f"To be tested: {n}")
-    if y:
-      print(f"(O) {n} is a prime number.")
-    else:
-      print(f"(X) {n} is NOT a prime number.")
+	if n <= 0:
+		print("Number must be non-zero, non-negative!")
+	else:
+		print("Checking...")
+		y = isprime(n)
+		print(f"To be tested: {n}")
+		if y:
+			print(f"(O) {n} is a prime number.")
+		else:
+			print(f"(X) {n} is NOT a prime number.")
--- primefuncs.py
+++ primefuncs.py
@@ -1,125 +1,125 @@
 import math
 
 def factorize(n):
-  '''Check out all the prime factors of a given number.'''
+	'''Check out all the prime factors of a given number.'''
 
-  limit = int(math.sqrt(n))  # negativity flagged, if any
-  primes = []
-  k = 6
+	limit = int(math.sqrt(n))  # negativity flagged, if any
+	primes = []
+	k = 6
 
-  # trivial case(s)
-  if n == 0 or n == 1:
-    return primes
+	# trivial case(s)
+	if n == 0 or n == 1:
+		return primes
 
-  c = 0
-  while True:
-    if not n%2 == 0:
-      break
-    c = c + 1
-    n = n // 2
+	c = 0
+	while True:
+		if not n%2 == 0:
+			break
+		c = c + 1
+		n = n // 2
 
-  if not c == 0:
-    primes.append((2, c))
-    if n == 1:
-      return primes  # only 2, early return
+	if not c == 0:
+		primes.append((2, c))
+		if n == 1:
+			return primes  # only 2, early return
 
-  c = 0
-  while True:
-    if not n%3 == 0:
-      break
-    c = c + 1
-    n = n // 3
+	c = 0
+	while True:
+		if not n%3 == 0:
+			break
+		c = c + 1
+		n = n // 3
 
-  if not c == 0:
-    primes.append((3, c))
-    if n == 1:
-      return primes  # only 3, early return
+	if not c == 0:
+		primes.append((3, c))
+		if n == 1:
+			return primes  # only 3, early return
 
-  while True:
-    # loop with steps of 6
+	while True:
+		# loop with steps of 6
 
-    j = k - 1
-    if not j <= limit:
-      primes.append((n, 1))  # the largest one
-      break
+		j = k - 1
+		if not j <= limit:
+			primes.append((n, 1))  # the largest one
+			break
 
-    c = 0
-    while True:
-      if not n%j == 0:
-        break
-      c = c + 1
-      n = n // j
+		c = 0
+		while True:
+			if not n%j == 0:
+				break
+			c = c + 1
+			n = n // j
 
-    if not c == 0:
-      primes.append((j, c))
-      if n == 1:
-        break  # all found already
+		if not c == 0:
+			primes.append((j, c))
+			if n == 1:
+				break  # all found already
 
-    j = k + 1
-    if not j <= limit:
-      primes.append((n, 1))  # the largest one
-      break
-  
-    c = 0
-    while True:
-      if not n%j == 0:
-        break
-      c = c + 1
-      n = n // j
+		j = k + 1
+		if not j <= limit:
+			primes.append((n, 1))  # the largest one
+			break
+	
+		c = 0
+		while True:
+			if not n%j == 0:
+				break
+			c = c + 1
+			n = n // j
  
-    if not c == 0:
-      primes.append((j, c))
-      if n == 1:
-        break  # all found already
+		if not c == 0:
+			primes.append((j, c))
+			if n == 1:
+				break  # all found already
 
-    # one small step of 6
-    k = k + 6
+		# one small step of 6
+		k = k + 6
 
-  # return value, a list
-  return primes
+	# return value, a list
+	return primes
 
 
 def isprime(n):
-  '''See if a given number is prime or not.'''
+	'''See if a given number is prime or not.'''
 
-  limit = int(math.sqrt(n))  # negativity flagged, if any
-  k = 6
-  status = True
+	limit = int(math.sqrt(n))  # negativity flagged, if any
+	k = 6
+	status = True
 
-  # trivial case(s)
-  if n == 0 or n == 1:
-    return False
+	# trivial case(s)
+	if n == 0 or n == 1:
+		return False
 
-  # not so trivial cases
-  if n == 2 or n == 3:
-    return True
+	# not so trivial cases
+	if n == 2 or n == 3:
+		return True
 
-  if n%2 == 0 or n%3 == 0:
-    return False
+	if n%2 == 0 or n%3 == 0:
+		return False
 
-  while True:
-    # loop with steps of 6
+	while True:
+		# loop with steps of 6
 
-    j = k - 1
-    if not j <= limit:
-      # itself a prime
-      break
+		j = k - 1
+		if not j <= limit:
+			# itself a prime
+			break
 
-    if n%j == 0:
-      status = False
-      break
+		if n%j == 0:
+			status = False
+			break
 
-    j = k + 1
-    if not j <= limit:
-      # itself a prime
-      break
+		j = k + 1
+		if not j <= limit:
+			# itself a prime
+			break
 
-    if n%j == 0:
-      status = False
-      break
+		if n%j == 0:
+			status = False
+			break
 
-    # one small step of 6
-    k = k + 6
+		# one small step of 6
+		k = k + 6
 
-  # primality determined
-  return status
+	# primality determined
+	return status
